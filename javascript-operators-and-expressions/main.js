9730c5783ed966ed4d285bfd981f4470f4e288d0
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-01-30 20:15:30 +0000

    Completed js-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,65 @@
+var width = 42;
+
+var height = 7;
+
+var area = width * height;
+
+console.log('Area is:', area);
+
+console.log('Area data type:', typeof area);
+
+var bill = 72;
+
+var payment = 88;
+
+var change = payment - bill;
+
+console.log('Your change is:', change);
+
+console.log('Change is:', typeof change);
+
+var quizzes = 60;
+
+var midterm = 67;
+
+var final = 92;
+
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('Final grade:', grade);
+
+console.log('Grade is a:', typeof grade);
+
+var firstName = 'Kristian';
+
+var lastName = 'Krog';
+
+var fullName = firstName + ' ' + lastName;
+
+console.log('My name is:', fullName);
+
+console.log('fullName is a:', typeof fullName);
+
+var ph = 1;
+
+var isAcidic = ph < 7;
+
+console.log('Is this acidic:', isAcidic);
+
+console.log('isAcidic is:', typeof isAcidic);
+
+var headCount = 12;
+
+var isSparta = headCount === 300;
+
+console.log('Is this Sparta?', isSparta);
+
+console.log('isSparta is:', typeof isSparta);
+
+var motto = fullName;
+
+motto += ' is the GOAT';
+
+console.log('The motto is:', motto);
+
+console.log('motto type:', typeof motto);

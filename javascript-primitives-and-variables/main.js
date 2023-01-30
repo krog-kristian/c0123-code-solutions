bcb401151b7cdba7170a17a46a329a9509f60f76
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-01-30 18:24:46 +0000

    Completed javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,29 @@
+var fullName = 'Kristian Krog';
+
+var isCool = true;
+
+var totalPets = 1;
+
+console.log('Full name:', fullName);
+
+console.log('Am cool:', isCool);
+
+console.log('Pet\'s I live with:', totalPets);
+
+console.log('fullName:', typeof fullName);
+
+console.log('isCool:', typeof isCool);
+
+console.log('totalPets:', typeof totalPets);
+
+var unknown = null;
+
+console.log('This unknown thing:', unknown);
+
+console.log('unknown:', typeof unknown);
+
+var never;
+
+console.log('I didn\'t assign a value:', never);
+
+console.log('never:', typeof never);

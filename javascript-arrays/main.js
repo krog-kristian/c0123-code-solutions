e088b96bc28c0c49e2d1f8cdc435341852af36c7
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-01-31 00:25:20 +0000

    Completed javascript-arrays.

---
+++ javascript-arrays/main.js
@@ -0,0 +1,29 @@
+var colors = ['red', 'white', 'blue'];
+
+console.log('value of colors at [0]:', colors[0]);
+
+console.log('value of colors at [1]:', colors[1]);
+
+console.log('value of colors at [2]:', colors[2]);
+
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+
+colors[2] = 'green';
+
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+
+console.log('value of colors:', colors);
+
+var students = ['Jimmy', 'John', 'Jason', 'Jack'];
+
+var numberOfStudents = students.length;
+
+console.log('There are ' + numberOfStudents + ' students that have a name starting with J.');
+
+var lastIndex = numberOfStudents - 1;
+
+var lastStudent = students[lastIndex];
+
+console.log('This is the last one, ' + lastStudent + ' to join the class.');
+
+console.log('value of students', students);

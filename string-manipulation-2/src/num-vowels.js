bbcdad4cae6c6bb2c5caff9211d8159f04f21f3b
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-02-05 20:05:43 +0000

    Completed string-manipulation-2

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,33 @@
 /* exported numVowels */
+/* define a function numVowels with one parameter string
+create a variable count and assign it the value of 0
+create another variable the string toLowerCase assigned to test variabe;
+loop through the test
+create a switch statement with a case for each vowel lowercase
+if true add 1 to the count
+return the count */
+
+function numVowels(string) {
+  var count = 0;
+  var test = string.toLowerCase();
+  for (var i = 0; i < string.length; i++) {
+    switch (test[i]) {
+      case 'a':
+        count += 1;
+        break;
+      case 'e':
+        count += 1;
+        break;
+      case 'i':
+        count += 1;
+        break;
+      case 'o':
+        count += 1;
+        break;
+      case 'u':
+        count += 1;
+        break;
+    }
+  }
+  return count;
+}
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,8 +1,15 @@
 /* exported ransomCase */
 
 /* define a function named ransomCase with 1 parameter string
-
-*/
+create a variable assigned to an empty string.
+loop throught the string start to end
+if the index  plus 1 is even
+create a variable assigned the the current iteration of the string sliced
+concatenate the variable a uppercased to new string
+else if odd
+assign the sliced index of string to a variable b
+concatenate the lowercased version to the newString
+return new string */
 
 function ransomCase(string) {
   var newString = '';

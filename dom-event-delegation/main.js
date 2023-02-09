7e9c389542d98fa0e52f7d68f9384ff56c5d5cf1
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-02-09 17:57:08 +0000

    Added tagName console.log

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -4,6 +4,7 @@ taskList.addEventListener('click', taskCheck);
 
 function taskCheck(event) {
   console.log('target:', event.target);
+  console.log('target tag:', event.target.tagName);
   if (event.target.matches('button')) {
     var ancestor = event.target.closest('.task-list-item');
     console.log('target parent:', ancestor);

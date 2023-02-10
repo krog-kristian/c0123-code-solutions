b590da1bc308824fc591eaa555ad7e654b846645
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-02-10 20:23:59 +0000

    completed players

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,22 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+var players = [
+  {
+    name: 'Cody',
+    hand: null
+  },
+  {
+    name: 'Rex',
+    hand: null
+  },
+  {
+    name: 'Anakin',
+    hand: null
+  },
+  {
+    name: 'Kenobi',
+    hand: null
+  }
+];
+
+console.log(players);

265573ef8e6df99c7e383696b09028ea9e5b422c
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-02-10 18:13:26 +0000

    completed up to difference.

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,20 @@
 /* exported unique */
+/* define a function named unique with one paramter an array
+create an array literal assigned to uniqueArray
+create a loop for array from 0 index to last index
+if uniqueArray passed through the includes method of the current array index value
+do nothing
+else push it into the uniqueArray
+after return the uniqueArray */
+
+function unique(array) {
+  var uniqueArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (uniqueArray.includes(array[i])) {
+      continue;
+    } else {
+      uniqueArray.push(array[i]);
+    }
+  }
+  return uniqueArray;
+}

4412ab1fd773b5427b556fc1b640310ae023d8ef
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-02-10 20:25:41 +0000

    added if statement

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -3,8 +3,10 @@
 var todos = [];
 var previousTodos = localStorage.getItem('javascript-local-storage');
 // console.log('retrieving todos', previousTodos);
-todos = JSON.parse(previousTodos);
-
+if (previousTodos !== null) {
+  todos = JSON.parse(previousTodos);
+}
+// if statement helpes prevent corrupted data
 window.addEventListener('beforeunload', function (event) {
   var todosJSON = JSON.stringify(todos);
   localStorage.setItem('javascript-local-storage', todosJSON);

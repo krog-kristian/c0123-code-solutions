202f77993748d6dc1b94069796a46a14dfab9947
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-03-16 18:49:47 +0000

    Updated to better versions from comments.

--- array-map/main.js
+++ array-map/main.js
@@ -11,11 +11,11 @@ const languages = [
 const doubled = numbers.map((num) => num * 2);
 console.log('All numbers doubled:', doubled);
 
-const prices = numbers.map((num) => `$${num}.00`);
+const prices = numbers.map((num) => `$${num.toFixed(2)}`);
 console.log('Prices array:', prices);
 
 const upperCased = languages.map((lang) => lang.toUpperCase());
 console.log('Languages uppercased:', upperCased);
 
-const firstLetters = languages.map((lang) => lang.slice(0, 1));
+const firstLetters = languages.map((lang) => lang[0]);
 console.log('All the first letters:', firstLetters);

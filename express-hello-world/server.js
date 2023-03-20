656e6d2d7d744487e4e75be71a12ca6ca6f5171d
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-03-20 21:03:56 +0000

    Fixed typo for h1 closing tag.

--- express-hello-world/server.js
+++ express-hello-world/server.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.use('/', (req, res) => {
+const response = (req, res) => {
   console.log('The method of req:', req.method);
-  res.send('<h1> Hello world<h1><p>Writing html like this would suck, prolly why we are gonna learn react</p>');
-});
+  res.send('<h1> Hello world</h1><p>Writing html like this would suck, prolly why we are gonna learn react</p>');
+};
+
+app.use('/', response);
 
 app.listen(8080, () => {
   console.log('Server start, listening on port 8080');

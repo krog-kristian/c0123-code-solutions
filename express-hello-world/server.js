0704194480bf6e6ebac5d8e6746b7396061ff07b
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-03-20 20:49:43 +0000

    Reorganized to follow proper conventions.

--- express-hello-world/server.js
+++ express-hello-world/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+
 const app = express();
-app.listen(8080, () => {
-  console.log('Server start, listening on port 8080');
-});
 
 app.use('/', (req, res) => {
   console.log('The method of req:', req.method);
-  res.send('<h1> Hello world</h1><p>Writing html like this would suck, prolly why we are gonna learn react</p>');
+  res.send('<h1> Hello world<h1><p>Writing html like this would suck, prolly why we are gonna learn react</p>');
+});
+
+app.listen(8080, () => {
+  console.log('Server start, listening on port 8080');
 });

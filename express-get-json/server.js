e2e624d7621130c0619178f8e77edceeb2a6fc48
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-03-20 22:43:07 +0000

    Send a response of json back for a particular path request.

---
+++ express-get-json/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+const getGrades = (req, res, next) => {
+  const json = [];
+  for (const key in grades) {
+    json.push(grades[key]);
+  }
+  next(sendBack(res, json));
+};
+
+const sendBack = (res, info) => {
+  res.json(info);
+};
+
+app.get('/api/grades', getGrades);
+
+app.listen(8080, () => {
+  console.log('Server start: port 8080.');
+});

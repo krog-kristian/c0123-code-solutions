5f21df7ffd9eccae3d1d6b5fe70697316e1e7a11
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-03-21 17:00:11 +0000

    Sent json to the server and saved it on the server.

---
+++ express-post-json/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+const app = express();
+const jsonParser = express.json();
+
+let nextId = 1;
+
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const json = [];
+  for (const key in grades) {
+    json.push(grades[key]);
+  }
+  res.json(json);
+});
+
+app.use(jsonParser);
+
+app.post('/api/grades', (req, res) => {
+  const newEntry = req.body;
+  newEntry.id = nextId;
+  grades[nextId] = newEntry;
+  nextId++;
+  res.status(201).json(newEntry);
+});
+
+app.listen(8080, () => {
+  console.log('Express listening on 8080.');
+});

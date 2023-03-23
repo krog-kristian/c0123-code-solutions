4964423da2f368f40c7b775f89fb1fb881e2e51e
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-03-23 17:26:46 +0000

    Used join to search values across multiple tables.

---
+++ sql-join/magic-mallrats.sql
@@ -0,0 +1,8 @@
+select "c"."firstName",
+       "c"."lastName",
+       "f"."title"
+from "customers" as "c"
+join "rentals" using ("customerId")
+join "inventory" using ("inventoryId")
+join "films" as "f" using ("filmId")
+where "title" = 'Magic Mallrats';

e5b743841c9b0dbc4e896456e415750acc5e93db
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-04-04 21:35:30 +0000

    Made suggested changes and added server error response message to error messages.

--- fetch-in-react-practice/client/src/components/Todos.js
+++ fetch-in-react-practice/client/src/components/Todos.js
@@ -34,59 +34,51 @@ export default function Todos() {
  * Adds a new todo to the todos via fetch
  * @param {string} newTodo
  */
-  function addTodo(newTodo) {
-    const todo = JSON.stringify(newTodo)
-    const request = new Request(url('/api/todos'), {
-      method: 'POST',
-      body: todo,
-      headers: {
-        'Content-Type': "application/json"
+  async function addTodo(newTodo) {
+      try {
+        const todo = JSON.stringify(newTodo)
+        const response = await fetch(url('/api/todos'), {
+          method: 'POST',
+          body: todo,
+          headers: {
+            'Content-Type': 'application/json'
+        }})
+      if (!response.ok) {
+        const serverError = await response.json();
+        console.log('Server msg:', serverError)
+        throw new Error(`Could not post: ${serverError.error}, code ${response.status}`);
       }
-    })
-    async function postTodo(outgoing) {
-      try{
-      const response = await fetch(outgoing)
-      if (!response.ok) throw new Error(`Could not post: ${response.status}`);
       const data = await response.json()
       setTodos((prev) => prev.concat(data))
       } catch (err) {
         setError(err)
       }
-    }
-    postTodo(request)
   }
 
 /**
  * Sends a request to change the isCompleted status of a todo.
  * @param {number} todoId of the item to be checked off.
  */
-  function toggleCompleted(todoId) {
-    const [todo] = todos.filter(item => item.todoId === todoId)
-    const value = {isCompleted: !todo.isCompleted}
-    const request = new Request(url(`/api/todos/${todoId}`), {
-      method: 'PATCH',
-      body: JSON.stringify(value),
-      headers: {
-        'Content-Type': "application/json"
-      }
-    })
-    async function postTodo(outgoing) {
+  async function toggleCompleted(todoId) {
       try {
-        const response = await fetch(outgoing)
-        if (!response.ok) throw new Error(`Could not update status: ${response.status}`);
-        const data = await response.json()
-        setTodos(todos.map((todo) => {
-          if (todo.todoId === todoId) {
-            return data;
-          } else {
-            return todo
+        const [todo] = todos.filter(item => item.todoId === todoId)
+        const value = { isCompleted: !todo.isCompleted }
+        const response = await fetch(url(`/api/todos/${todoId}`), {
+          method: 'PATCH',
+          body: JSON.stringify(value),
+          headers: {
+            'Content-Type': 'application/json'
           }
-        }))
+        })
+        if (!response.ok) {
+          const serverError = await response.json();
+          console.log('Server msg:', serverError)
+         throw new Error(`Could not update status: ${serverError.error}, code ${response.status}`);}
+        const data = await response.json()
+        setTodos(todos.map(todo => todo.todoId === todoId ? data : todo))
       } catch (err) {
         setError(err)
       }
-    }
-    postTodo(request)
   }
 
   if (isLoading) {

cc4b4b21d1c653fbd901d5562d204661cf80a63f
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-04-05 21:59:28 +0000

    Added authorization middleware to validate tokens.

---
+++ api-authorization/server/lib/authorization-middleware.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars -- Remove me */
+import jwt from 'jsonwebtoken';
+import { ClientError } from './client-error.js';
+
+export async function authorizationMiddleware(req, res, next) {
+  try {
+    const authorization = req.get('authorization');
+    if (!authorization) throw new ClientError(401, 'Authorization Required.');
+    const token = authorization.split('Bearer ')[1];
+    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = payload;
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
+
+/**
+  * References:
+  * https://expressjs.com/en/4x/api.html#req.get
+  * https://nodejs.org/api/http.html#http_message_headers
+  * https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
+  */

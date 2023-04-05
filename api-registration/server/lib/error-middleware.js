c747766bd4f5d6294c16bc3d872eda5ee2e6f64c
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-04-05 18:05:46 +0000

    Added error handling for username exists.

--- api-registration/server/lib/error-middleware.js
+++ api-registration/server/lib/error-middleware.js
@@ -4,7 +4,12 @@ export function errorMiddleware(err, req, res, next) {
   if (err instanceof ClientError) {
     res.status(err.status).json({ error: err.message });
   } else {
-    console.error(err);
-    res.status(500).json({ error: 'an unexpected error occurred' });
+    if (err.code === '23505') {
+      console.error(err);
+      res.status(403).json({ error: 'Username already exists!' });
+    } else {
+      console.error(err);
+      res.status(500).json({ error: 'an unexpected error occurred' });
+    }
   }
 }

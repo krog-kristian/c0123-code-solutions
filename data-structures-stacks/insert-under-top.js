c5e318372ec19e39fb4673e1b296f248b453854c
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-04-12 17:04:51 +0000

    Completed final two tests.

--- data-structures-stacks/insert-under-top.js
+++ data-structures-stacks/insert-under-top.js
@@ -1,2 +1,9 @@
 export default function insertUnderTop(stack, value) {
+  const data = stack.peek();
+  if (data === undefined) {
+    return;
+  }
+  const top = stack.pop();
+  stack.push(value);
+  stack.push(top);
 }

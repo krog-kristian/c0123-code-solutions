7d5430933d66e12cc08470add9cbf2977e798e92
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-04-13 20:54:25 +0000

    Removed second loop from remove-tail.

--- data-structures-linked-lists/remove-tail.js
+++ data-structures-linked-lists/remove-tail.js
@@ -1,14 +1,11 @@
 export default function removeTail(list) {
   if (list.next === null) return;
-  let count = 0;
-  let links = list;
-  while (links.next !== null) {
-    count++;
-    links = links.next;
-  }
-  while (count > 1) {
-    count--;
+  while (list.next !== null) {
+    const link = list.next;
+    if (link.next === null) {
+      list.next = null;
+      return;
+    }
     list = list.next;
   }
-  list.next = null;
 }
